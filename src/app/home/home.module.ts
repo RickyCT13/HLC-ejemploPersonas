059ef37da466ee6823b99537e3691cbc8f33bdf2
RickyCT13 <ricardomorenocantea13@gmail.com>
2023-12-14 12:09:46 +0100

    Primer commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
@@ -10,6 +11,7 @@ import { HomePageRoutingModule } from './home-routing.module';
 @NgModule({
   imports: [
     CommonModule,
+    HttpClientModule,
     FormsModule,
     IonicModule,
     HomePageRoutingModule
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  users: any;
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {
+	this.users = this.httpClient.get('https://randomuser.me/api?results=20').pipe(map((res: any) => res['results']));
 
+  }
 }
